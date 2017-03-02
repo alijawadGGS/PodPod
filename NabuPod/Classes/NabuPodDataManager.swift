53ceb0da8a44fcbc07cdf183c8118cc67873e8f2
Ali Jawad <alijawad@AliJawad.local>
2017-03-02 16:27:22 +0500

    testing

---
+++ NabuPod/Classes/NabuPodDataManager.swift
@@ -0,0 +1,17 @@
+//
+//  NabuPodDataManager.swift
+//  Pods
+//
+//  Created by Ali Jawad on 02/03/2017.
+//
+//
+
+import Foundation
+
+class NabuPodDataManager
+{
+    class func testMethod()
+    {
+        print("Yes it is working")
+    }
+}
