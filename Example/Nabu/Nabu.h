cec7d6ae2d4bc7833d4bc6fc94a3e10a8da2a1d1
Ali Jawad <alijawad@AliJawad.local>
2017-03-02 23:57:25 +0500

    m

---
+++ Example/Nabu/Nabu.h
@@ -0,0 +1,19 @@
+//
+//  Nabu.h
+//  Nabu
+//
+//  Created by Ali Jawad on 02/03/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nabu.
+FOUNDATION_EXPORT double NabuVersionNumber;
+
+//! Project version string for Nabu.
+FOUNDATION_EXPORT const unsigned char NabuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nabu/PublicHeader.h>
+
+
---
+++ NabuPod/Classes/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Pods
+//
+//  Created by Ali Jawad on 02/03/2017.
+//
+//
+
+#ifndef Test_h
+#define Test_h
+
+
+#endif /* Test_h */
