8b49cec6eb8d32b1e81c92242787e2f6b468ce50
Ali Jawad <alijawad@AliJawad.local>
2017-03-02 17:08:17 +0500

    0.1.3

---
+++ Example/TemClass.swift
@@ -0,0 +1,17 @@
+//
+//  TemClass.swift
+//  NabuPod
+//
+//  Created by Ali Jawad on 02/03/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+class TemClass
+{
+    class func tem()
+    {
+    
+    }
+}
