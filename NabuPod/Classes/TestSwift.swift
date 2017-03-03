5c347eb8ca63dbe06e27a676307d641371521dad
Ali Jawad <alijawad@AliJawad.local>
2017-03-03 10:43:44 +0500

    m

---
+++ NabuPod/Classes/TestSwift.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwift.swift
+//  Pods
+//
+//  Created by Ali Jawad on 03/03/2017.
+//
+//
+
+import Foundation
+
+class TestSwift: NSObject {
+    
+    class func tem()
+    {
+        print("Tem is working")
+    }
+}
